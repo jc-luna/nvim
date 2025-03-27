f91787ed104cb9bb587429361ee7e525b2d9518e
jc-luna <jc.lunaa@protonmail.com>
2025-03-27 11:22:49 -0500

    vimwiki-like plugin added and treesitter fixed

--- lua/plugins/specs/treesitter.lua
+++ lua/plugins/specs/treesitter.lua
@@ -1,3 +1,14 @@
 return {
-    "nvim-treesitter/nvim-treesitter"
+    "nvim-treesitter/nvim-treesitter",
+	config = function()
+		local cfg = require("nvim-treesitter.configs")
+		cfg.setup({
+			ensure_installed = {
+				"c", "lua",
+				"vim", "vimdoc",
+				"query", "markdown",
+				"markdown_inline"
+			}
+		})
+	end
 }
