dbedfb71db1fd9e64650330270c093a0ddd990bb
jc-luna <jc.lunaa@protonmail.com>
2025-03-27 11:12:21 -0500

    snippets?

--- lua/plugins/configs/cmp/init.lua
+++ lua/plugins/configs/cmp/init.lua
@@ -1,15 +1,53 @@
 local cmp = require("cmp")
+local luasnip = require("luasnip")
+
 cmp.setup({
-   sources = cmp.config.sources({
-	   { name = "nvim_lsp",
-            entry_filter = function(entry, ctx)
-                return require("cmp").lsp.CompletionItemKind.Text ~= entry:get_kind()
-            end },
+	sources = cmp.config.sources({
+	{
+		name = "nvim_lsp",
+        entry_filter = function(entry, ctx)
+            return require("cmp").lsp.CompletionItemKind.Text ~= entry:get_kind()
+        end
+	},
+	{
+		name = "luasnip",
+	},
 	}),
    mapping = cmp.mapping.preset.insert({
 	   ["<C-Space>"] = cmp.mapping.complete(),
 	   ["<C-u>"] = cmp.mapping.scroll_docs(-4),
 	   ["<C-d>"] = cmp.mapping.scroll_docs(4),
-	   ["<tab>"] = cmp.mapping.confirm({ select = true }),
+	   ["<Tab>"] = cmp.mapping.confirm({ select = true }),
+	   ["<C-j>"] =	cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_next_item()
+			elseif luasnip.expand_or_jumpable() then
+				luasnip.expand_or_jump()
+			elseif has_words_before() then
+				cmp.complete()
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
+		["<C-k>"] =	cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_prev_item()
+			elseif luasnip.expand_or_jumpable() then
+				luasnip.expand_or_jump()
+			elseif has_words_before() then
+				cmp.complete()
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
+
    }),
+   snippet = {
+		expand = function(args)
+			local ls = prequire("luasnip")
+			if not ls then return end
+
+			ls.lsp_expand(args.body)
+		end
+   }
 })
--- lua/plugins/specs/lua-snippets.lua
+++ lua/plugins/specs/lua-snippets.lua
@@ -1,5 +1,6 @@
 return {
 	"L3MON4D3/LuaSnip",
+	after = "nvim-cmp",
 	version = "v2.*",
 	build = "make install_jsregexp"
 }
