7c843e7f4139d8af742ef697e8578e5c507487db
jc-luna <jc.lunaa@protonmail.com>
2025-04-08 18:50:24 -0500

    reorganization! and install telescope.nvim

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -2,21 +2,24 @@
 return {
 	require("plugins.themes.catppuccin.init"),
 
-	require("plugins.specs.lualine"),
-	require("plugins.specs.neo-tree"),
+	require("plugins.completion.cmp"),
+	require("plugins.completion.lsp"),
+	require("plugins.completion.lua-snippets"),
+	require("plugins.completion.treesitter"),
+
+	require("plugins.navigation.neo-tree"),
+	require("plugins.navigation.lualine"),
+	require("plugins.navigation.telescope"),
+	require("plugins.navigation.homekey"),
+
 	require("plugins.specs.vimtex"),
-	require("plugins.specs.homekey"),
 	require("plugins.specs.mini-surround"),
 	require("plugins.specs.kiwi"),
 
 	require("plugins.specs.cmake-tools"),
-	require("plugins.specs.cmp"),
-	require("plugins.specs.lsp"),
 
 --[[	require("plugins.specs.roblox.rojo"),
 	require("plugins.specs.roblox.luau-lsp"),
 	require("plugins.specs.roblox.luau-tree"),]]--
 
-	require("plugins.specs.lua-snippets"),
-	require("plugins.specs.treesitter")
 }
