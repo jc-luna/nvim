5f0401e6e647de1b3f02608de15901e2a1142b65
jc-luna <jc.lunaa@protonmail.com>
2025-04-11 17:51:28 -0500

    kiwi the conf

--- lua/plugins/configs/kiwi.lua
+++ lua/plugins/configs/kiwi.lua
@@ -1,19 +1,27 @@
 return {
     "serenevoid/kiwi.nvim",
+
     opts = {
         {
-            name = "work",
-            path = "work-wiki"
-        },
-        {
-            name = "personal",
-            path = "personal-wiki"
+            name = "copypasta",
+            path = "~/documents/copypasta/"
         }
     },
     keys = {
-        { "<leader>ww", ":lua require(\"kiwi\").open_wiki_index()<cr>", desc = "Open Wiki index" },
-        { "<leader>wp", ":lua require(\"kiwi\").open_wiki_index(\"personal\")<cr>", desc = "Open index of personal wiki" },
-        { "T", ":lua require(\"kiwi\").todo.toggle()<cr>", desc = "Toggle Markdown Task" }
+        {
+        	"<leader>ww",
+        	":lua require(\"kiwi\").open_wiki_index()<cr>",
+        	desc = "Open Wiki index"
+        },
+        {
+        	"<leader>wp",
+        	":lua require(\"kiwi\").open_wiki_index(\"personal\")<cr>",
+        	desc = "Open index of personal wiki"
+        },
+        {
+        	"T", ":lua require(\"kiwi\").todo.toggle()<cr>",
+        	desc = "Toggle Markdown Task"
+        }
     },
     lazy = true
 }
