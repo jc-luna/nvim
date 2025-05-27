a87b94359f037aed8b41df4be58fc8a6d150a715
jc-luna <jc.lunaa@protonmail.com>
2025-05-26 23:13:55 -0500

    gitignore snippet

--- lua/settings/plugin.lua
+++ lua/settings/plugin.lua
@@ -16,6 +16,7 @@ ls.add_snippets("tex", require("snippet.latex.preamble"))
 ls.add_snippets("tex", require("snippet.latex.macros"))
 ls.add_snippets("tex", require("snippet.latex.titlepage"))
 ls.add_snippets("markdown", require("snippet.markdown"))
+ls.add_snippets("gitignore", require("snippet.gitignore"))
 
 vim.filetype.add({
 	extension = {
---
+++ lua/snippet/gitignore/init.lua
@@ -0,0 +1,21 @@
+local ls = require("luasnip")
+
+return {
+	ls.snippet({
+		trig = "latex",
+		docstring = "Ignore common LaTeX build things."
+	}, {
+		ls.text_node({
+			"# Ignore LaTeX Build Things",
+			"*.aux",
+			"*.fdb_latexmk",
+			"*.log",
+			"*.dvi",
+			"*.fls",
+			"*.synctex.gz",
+			"*.out.ps",
+			"*.synctex(busy)",
+			"*.pdf"
+		})
+	})
+}
