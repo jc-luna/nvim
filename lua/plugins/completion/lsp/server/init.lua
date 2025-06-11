c735348a4200fd56d4cad10070516c8219bc0e52
jc-luna <jc.lunaa@protonmail.com>
2025-06-11 06:13:11 -0500

    the html and css lsp

--- lua/plugins/completion/lsp/server/init.lua
+++ lua/plugins/completion/lsp/server/init.lua
@@ -1,10 +1,15 @@
 local lspconfig = require("lspconfig")
 
 return {
-	require("plugins.completion.lsp.server.clangd"),
-	require("plugins.completion.lsp.server.lua"),
 	require("plugins.completion.lsp.server.python"),
 	require("plugins.completion.lsp.server.latex"),
+
+	require("plugins.completion.lsp.server.clangd"),
 	require("plugins.completion.lsp.server.cmake"),
-	require("plugins.completion.lsp.server.luau")
+
+	require("plugins.completion.lsp.server.lua"),
+	require("plugins.completion.lsp.server.luau"),
+
+	require("plugins.completion.lsp.server.html"),
+	require("plugins.completion.lsp.server.css")
 }
