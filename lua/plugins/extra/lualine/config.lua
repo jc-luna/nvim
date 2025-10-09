856bd0f6d846612680d20bdce3a18ed8b3884d02
JC Luna <jc.lunaa@protonmail.com>
2025-10-09 14:01:02 -0500

    Lualine tweaks to remove unused bits

--- lua/plugins/extra/lualine/config.lua
+++ lua/plugins/extra/lualine/config.lua
@@ -3,8 +3,10 @@ return function()
 		options = {
 			theme = "catppuccin",
 			icons_enabled = true,
-			component_separators = { left = "/", right = "/" },
-			section_separators = { left = "◤", right = "◥" },
+			--component_separators = { left = "/", right = "/" },
+			--section_separators = { left = "◤", right = "◥" },
+			component_separators = { left = "|", right = "|" },
+			section_separators = { left = "▌", right = "▐" },
 			disabled_filetypes = {
 				statusline = {},
 				winbar = {},
@@ -20,18 +22,18 @@ return function()
 			}
 		},
 		sections = {
-			lualine_a = {'mode'},
-			lualine_b = {'branch', 'diff', 'diagnostics'},
-			lualine_c = {'filename'},
-			lualine_x = {'encoding', 'fileformat', 'filetype'},
-			lualine_y = {'progress'},
-			lualine_z = {'location'}
+			lualine_a = {"mode"},
+			lualine_b = {"branch", "diagnostics"},
+			lualine_c = {},
+			lualine_x = {"encoding", "fileformat", "filetype"},
+			lualine_y = {"progress"},
+			lualine_z = {"location"}
 		},
 		inactive_sections = {
 			lualine_a = {},
 			lualine_b = {},
-			lualine_c = {'filename'},
-			lualine_x = {'location'},
+			lualine_c = {},
+			lualine_x = {"location"},
 			lualine_y = {},
 			lualine_z = {}
 		},
