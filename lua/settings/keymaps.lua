d43804fd7820a2fd6715010cb43376238f7a832f
JC Luna <jc.lunaa@protonmail.com>
2026-04-09 08:47:18 -0500

    Telescope mappings

--- lua/settings/keymaps.lua
+++ lua/settings/keymaps.lua
@@ -3,3 +3,6 @@ vim.keymap.set("n", "<Right>", "<Nop>", {noremap = true})
 vim.keymap.set("n", "<Up>", "<Nop>", {noremap = true})
 vim.keymap.set("n", "<Down>", "<Nop>", {noremap = true})
 
+local telebuilt = require("telescope.builtin")
+vim.keymap.set("n", "<A-f>", telebuilt.live_grep, {noremap = true})
+vim.keymap.set("n", "<A-F>", telebuilt.find_files, {noremap = true})
