018c35f0f6d4e46278dc36420501282a69f4741c
JC Luna <jc.lunaa@protonmail.com>
2026-04-20 08:31:08 -0500

    tabby theming updates

--- lua/plugins/themes/tabby/config.lua
+++ lua/plugins/themes/tabby/config.lua
@@ -5,6 +5,7 @@ local theme = {
 	fill = { --[[fg = "#11111b", ]]bg = "#11111b" },
 	head = { bg = "#585b70" },
 	current_tab = { bg = "#cba6f7" },
+	not_current_tab = { bg = "#9399b2" },
 	win = { bg = "#585b70" },
 	tab = { bg = "#313244" },
 	tail = { bg = "#585b70" },
@@ -15,26 +16,30 @@ require("tabby").setup({
 	line = function(line)
 		return {
 			{
+				--{ " luna ", hl = theme.head },
 				{ " luna ", hl = theme.head },
-				line.sep("", theme.head, theme.fill),
+				--line.sep("", theme.head, theme.fill),
+				line.sep("", theme.head, theme.fill),
 			},
 
 			line.tabs().foreach(function(tab)
 				return {
 					line.sep(
-						"█",
-						tab.is_current() and theme.current_tab or theme.tab,
+						--"█",
+						"█",
+						tab.is_current() and theme.current_tab or theme.not_current_tab,
 						theme.fill
 					),
 					{
-						tostring(tab.number())..(tab.is_current() and "!" or ""),
+						tostring(tab.number())..(tab.is_current() and "!" or " "),
 						hl = theme.tab
 					},
 					{
 						tab.name(),
 						hl = theme.tab
 					},
-					line.sep("█",
+					--line.sep("█",
+					line.sep("█",
 						theme.tab,
 						(tab.number() == #(line.tabs().tabs))
 							and theme.transparent or theme.fill),
@@ -46,8 +51,9 @@ require("tabby").setup({
 			line.spacer(),
 
 			{
-				line.sep("", theme.tail, theme.transparent),
-				{ " omg hai :3 ", hl = theme.tail },
+				--line.sep("◥", theme.tail, theme.transparent),
+				line.sep("", theme.tail, theme.transparent),
+				{ " :3 ", hl = theme.tail },
 			},
 			hl = theme.transparent,
 		}
